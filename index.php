3b5d1f7908e8ad8af3008638c3e77802294d4c18
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2023-12-21 07:16:45 +0530

    PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 10 + 10;
+
+echo "Satya Narayan Rout";
+
+$name = "Satyanarayan Rout";
+echo $name;
+?>
+
+<h1><?php echo $name;?></h1>
+
+<h1 style="color: red;"><?php echo $name;?></h1>
+
+<h1 style="color:<?php echo $color; ?>"><?php echo $name;?></h1>
