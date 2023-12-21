feb0654aeda93137e314bd6911b3256ed4a7b187
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2023-12-21 19:16:53 +0530

    Test

---
+++ variable.php
@@ -0,0 +1,40 @@
+<?php
+$name="Satya";
+echo $name;
+echo "<br />";
+
+echo $name;
+echo "<br />";
+
+echo $name;
+echo "<br />";
+
+echo $name;
+echo "<br />";
+
+echo $name;
+echo "<br />";
+
+echo $name;
+echo "<br />";
+
+echo $name;
+echo "<br />";
+
+$num = 120;
+echo $num + 10;
+echo "<br />";
+
+echo  $num * 10;
+echo "<br />";
+
+echo $num - 10;
+echo "<br />";
+
+echo $num/10;
+echo "<br />"
+
+$any = 1.1;
+
+echo $any;
+?>
