fc52646f8b6487442da0aace186b112693c7fea6
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2023-12-24 07:33:54 +0530

    php

--- echo.php
+++ echo.php
@@ -1,6 +1,12 @@
 <?php
 // ecHO "Itismita Bal";
-echo "Satya Narayan Rout ❤️ Itismita Bal "; echo "I Love Kunmun Very Much"; echo " 30 ❤️ 24"
-
+echo "Satya Narayan Rout ❤️ Itismita Bal ";
+echo "<br/>";
+echo "I Love Kunmun Very Much ";
+echo "<br/>";
+ echo " 30 ❤️ 24 <br/>";
+// echo as a function
+echo "<br/>";
+echo("as a function");
 
 ?>
